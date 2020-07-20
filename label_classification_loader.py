589f49d07b06ab65c77cee7bb883e4eec164b806
Lahiru <lahrumesh28@gmail.com>
2020-07-20 10:45:48 +0530

    split train and test

--- label_classification_loader.py
+++ label_classification_loader.py
@@ -6,11 +6,12 @@ import cv2
 import argparse
 import matplotlib.pyplot as plt
 
-REBUILD_DATA=False 
+REBUILD_DATA=True 
 
 parser=argparse.ArgumentParser()
 parser.add_argument("--Data_Folder", type=str, default="Data", help="Training Images folder")
 parser.add_argument("--IMAGE_SIZE", type=str, default=100, help="Image Size")
+parser.add_argument("--TEST_SIZE", type=float, default=0.4, help="Testing data percentage")
 args = parser.parse_args()
 
 
@@ -22,7 +23,7 @@ with open('classes.txt', 'w') as f:
 
 class Classification_Data():
 
-    training_data=[]
+    data_list=[]
     labels = {full_path[x]:x for x in range(len(full_path))}
     
 
@@ -32,15 +33,17 @@ class Classification_Data():
                 try:
                     path=os.path.join(label,f)
                     img=cv2.imread(path)
-                    print(self.labels[label])
                     img=cv2.resize(img,(args.IMAGE_SIZE,args.IMAGE_SIZE))
-                    self.training_data.append([np.array(img),self.labels[label]])
+                    self.data_list.append([np.array(img),self.labels[label]])
 
                 except Exception as e:
                     pass
 
-        np.random.shuffle(self.training_data)
-        np.save("training_data1.npy",self.training_data) #Save data to numpy array
+        np.random.shuffle(self.data_list)
+        test_size=int(len(self.data_list)*args.TEST_SIZE)
+        training_data,testing_data=self.data_list[test_size:],self.data_list[:test_size]
+        np.save("training_data.npy",training_data) #Save training data to numpy array
+        np.save("testing_data.npy",testing_data) #Save testing data to numpy array
       
 
 
@@ -49,6 +52,8 @@ if REBUILD_DATA:
     clas_data.generate_data()
 
 training_data=np.load("training_data1.npy",allow_pickle=True)
+#print(int(len(training_data)*0.5))
+#training, test = training_data[:80,:], training_data[80:,:]
 
 
 #print(training_data)
@@ -56,7 +61,7 @@ training_data=np.load("training_data1.npy",allow_pickle=True)
 #for i in training_data:
 #    print(i[1])
 
-print(training_data[7][1])
+#print(training_data[7][1])
 
-plt.imshow(training_data[7][0])
-plt.show()
+#plt.imshow(training_data[7][0])
+#plt.show()
